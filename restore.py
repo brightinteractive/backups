a409920c59de17e42e76042fb384ccc7add6461a
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-15 15:13:14 +0100

    Add get S3 bucket by name method.

--- restore.py
+++ restore.py
@@ -3,9 +3,9 @@
 import boto3
 
 class S3Resource(object):
-    def __init__(self):
-        session = create_aws_session()
-        resource = create_s3_resource(session)
+    def __init__(self, session=None, resource=None):
+        self.session = session or self.create_aws_session()
+        self.resource = resource or self.create_s3_resource(self.session)
 
     @classmethod
     def create_aws_session(cls):
@@ -15,4 +15,7 @@ class S3Resource(object):
     def create_s3_resource(cls, session=None):
         session = session or cls.create_aws_session()
         return session.resource('s3')
+
+    def get_s3_bucket_by_name(self, bucket_name):
+        return self.resource.Bucket(bucket_name)
 
--- tests.py
+++ tests.py
@@ -30,12 +30,34 @@ class ResourceTests(unittest.TestCase):
         mock_resource = object()
         mock_session = Mock()
         mock_session.resource = MagicMock(return_value=mock_resource)
+
         resource = S3Resource.create_s3_resource(mock_session)
         self.assertEqual(resource, mock_resource)
+
         mock_session.resource.assert_called_with('s3')
 
     def test_create_s3_resource_creates_new_session_if_one_is_not_passed_to_it(self):
         with patch.object(S3Resource, 'create_aws_session') as mock:
             S3Resource.create_s3_resource()
             mock.assert_called_once()
+
+    def test_get_s3_bucket_by_name_passes_expected_name_to_resource_Bucket_method(self):
+        test_name = object()
+        mock_resource = Mock()
+        mock_resource.Bucket = MagicMock()
+
+        resource = S3Resource(resource=mock_resource)
+        resource.get_s3_bucket_by_name(test_name)
+
+        mock_resource.Bucket.assert_called_with(test_name)
+
+    def test_get_s3_bucket_by_name_returns_result_of_resource_Bucket_method(self):
+        test_bucket = object()
+        mock_resource = Mock()
+        mock_resource.Bucket = MagicMock(return_value=test_bucket)
+
+        resource = S3Resource(resource=mock_resource)
+        bucket = resource.get_s3_bucket_by_name('test name')
+
+        self.assertEqual(test_bucket, bucket)
 
