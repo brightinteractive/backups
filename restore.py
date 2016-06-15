53062de48f575d23303022ac8fbca696060de682
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-15 13:45:58 +0100

    Create AWS resource using the session instance passed as parameter.

--- restore.py
+++ restore.py
@@ -4,3 +4,7 @@ import boto3
 
 def create_aws_session():
     return boto3.session.Session()
+
+def create_s3_resource(session):
+    return session.resource('s3')
+
--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@
 import unittest, os
 
 import boto3
-from mock import patch
+from mock import patch, Mock, MagicMock
 
 import restore
 
@@ -24,4 +24,11 @@ class RestoreTests(unittest.TestCase):
             credentials = session.get_credentials()
             self.assertEqual(test_credentials['AWS_ACCESS_KEY_ID'], credentials.access_key)
             self.assertEqual(test_credentials['AWS_SECRET_ACCESS_KEY'], credentials.secret_key)
+    def test_create_s3_resource_returns_an_aws_s3_resource_using_session_param(self):
+        mock_resource = object()
+        mock_session = Mock()
+        mock_session.resource = MagicMock(return_value=mock_resource)
+        resource = restore.create_s3_resource(mock_session)
+        self.assertEqual(resource, mock_resource)
+        mock_session.resource.assert_called_with('s3')
 
