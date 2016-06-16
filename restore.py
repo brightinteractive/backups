d1170dbcad245e4ab8c817b28780719bd4968ef5
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-16 12:49:17 +0100

    Remove default parameters for session and resource.

--- restore.py
+++ restore.py
@@ -3,17 +3,16 @@
 import boto3
 
 class S3Resource(object):
-    def __init__(self, session=None, resource=None):
-        self.session = session or self.create_aws_session()
-        self.resource = resource or self.create_s3_resource(self.session)
+    def __init__(self):
+        self.session = self.create_aws_session()
+        self.resource = self.create_s3_resource(self.session)
 
     @classmethod
     def create_aws_session(cls):
         return boto3.session.Session()
 
     @classmethod
-    def create_s3_resource(cls, session=None):
-        session = session or cls.create_aws_session()
+    def create_s3_resource(cls, session):
         return session.resource('s3')
 
     def get_s3_bucket_by_name(self, bucket_name):
