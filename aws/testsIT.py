bb355aa8ed1b5008b81357a54e633491c8347eed
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-20 15:42:18 +0100

    Integration tests for AWS API wrapper.

---
+++ aws/testsIT.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unittest, os, collections
+
+import boto3
+
+from aws import AWSApiWrapper
+
+
+class AWSApiWrapperTestsIT(unittest.TestCase):
+
+    def test__we_can_create_an_aws_session(self):
+        aws = AWSApiWrapper()
+        session = aws.create_aws_session()
+        self.assertIsInstance(session, boto3.session.Session)
+
+    def test__we_can_get_an_s3_resource(self):
+        aws = AWSApiWrapper()
+        resource = aws.create_s3_resource()
+        self.assertIsInstance(resource, boto3.resources.base.ServiceResource)
+
+    def test__we_can_get_all_objects_within_a_named_bucket(self):
+        ''' For this test to pass there must be a bucket called:
+            test-we-can-get-all-objects-from-a-named-bucket
+        and it must have 3 objects in it: one.txt, two.txt, three.txt
+        '''
+        bucket = 'test-we-can-get-all-objects-from-a-named-bucket'
+        expected_object_keys = set(['one.txt', 'two.txt', 'three.txt'])
+        aws = AWSApiWrapper()
+
+        objects = aws.get_s3_objects_by_bucket_name(bucket)
+
+        returned_object_keys = set([object.key for object in objects])
+        self.assertEqual(expected_object_keys, returned_object_keys)
+
