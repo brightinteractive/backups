afdb2ed568ce210472d0f4d305a10160c5938513
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-20 16:27:46 +0100

    Refactor AWS Api Wrapper Tests.

--- aws/aws.py
+++ aws/aws.py
@@ -23,3 +23,29 @@ class AWSApiWrapper(object):
         bucket = self.get_s3_bucket_by_name(bucket_name)
         return bucket.objects.all()
 
+
+class S3Restore(object):
+    RestoreRequest = { 'Days' : 7 }
+
+    @classmethod
+    def restore_s3_object(cls, s3_obj_summary):
+        if cls._object_should_be_restored(s3_obj_summary):
+            return cls.call_restore(s3_obj_summary)
+
+    @classmethod
+    def _object_should_be_restored(cls, s3_obj_summary):
+        return cls.is_glacier_type(s3_obj_summary) and cls.is_not_restored(s3_obj_summary)
+
+    @classmethod
+    def is_glacier_type(cls, s3_obj_summary):
+        return s3_obj_summary.storage_class == 'GLACIER'
+
+    @classmethod
+    def is_not_restored(cls, s3_obj_summary):
+        s3_obj_detail = s3_obj_summary.Object()
+        return s3_obj_detail.restore is None
+
+    @classmethod
+    def call_restore(cls, s3_obj_summary):
+        return s3_obj_summary.restore_object(RestoreRequest=cls.RestoreRequest)
+
---
+++ aws/tests.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import unittest, os, collections
+
+import boto3
+from mock import patch, Mock, MagicMock
+
+from aws import AWSApiWrapper, S3Restore
+
+
+class AWSApiWrapperTests(unittest.TestCase):
+
+    def test__we_can_create_an_aws_session_with_credentials_stored_in_environment_variables(self):
+        test_credentials = {
+                'AWS_ACCESS_KEY_ID':'test access key',
+                'AWS_SECRET_ACCESS_KEY':'test secret key'
+                }
+
+        with patch.dict('os.environ', test_credentials):
+            aws = AWSApiWrapper()
+            session = aws.create_aws_session()
+            credentials = session.get_credentials()
+            self.assertEqual(test_credentials['AWS_ACCESS_KEY_ID'], credentials.access_key)
+            self.assertEqual(test_credentials['AWS_SECRET_ACCESS_KEY'], credentials.secret_key)
+
+    def test__we_can_identify_an_s3_object_with_glacier_storage_class(self):
+        mock_s3_object = Mock()
+        mock_s3_object.storage_class = 'GLACIER'
+        self.assertTrue(S3Restore.is_glacier_type(mock_s3_object))
+
+        mock_s3_object.storage_class = 'STANDARD'
+        self.assertFalse(S3Restore.is_glacier_type(mock_s3_object))
+
+    def test__we_can_identify_an_s3_object_that_has_yet_to_be_restored(self):
+        mock_detail_object = Mock()
+        mock_summary_object = Mock()
+        mock_summary_object.Object = MagicMock(return_value=mock_detail_object)
+
+        mock_detail_object.restore = None
+        self.assertTrue(S3Restore.is_not_restored(mock_summary_object))
+
+        mock_detail_object.restore = 'returns a string is restore is in progress or completed'
+        self.assertFalse(S3Restore.is_not_restored(mock_summary_object))
+
+    def test__we_call_restore_when_we_identify_an_s3_object_that_is_glacier_storage_class_and_is_in_an_unrestored_state(self):
+        mock_detail_object = Mock()
+        mock_summary_object = Mock()
+        mock_summary_object.Object = MagicMock(return_value=mock_detail_object)
+
+        mock_summary_object.storage_class = 'GLACIER'
+        mock_detail_object.restore = None
+
+        with patch.object(S3Restore, 'call_restore') as call_to_restore:
+            S3Restore.restore_s3_object(mock_summary_object)
+            call_to_restore.assert_called_once()
+            call_to_restore.assert_called_with(mock_summary_object)
+
+        self.assertTrue(S3Restore.is_glacier_type(mock_summary_object))
