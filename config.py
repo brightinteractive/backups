5d1c1c223c694ee5ea7e438a1f08531cd697f11d
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-21 13:15:09 +0100

    Sample config file. This file is created by an Ansible Jinja template. Environment variables will be defined here at deploy time.

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+
+class EnvironmentVariables(object):
+    @staticmethod
+    def inject():
+        '''This file is created by an Ansible Jinja template. Environment variables are defined here at deploy time.
+        '''
+        pass
+
