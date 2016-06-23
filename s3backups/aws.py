6d4fea42768766f2b774046b9745af8f01abdb7c
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-23 13:44:08 +0100

    Use producer/consumer threads to make AWS restore requests

--- s3backups/aws.py
+++ s3backups/aws.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+from Queue import Queue
+from threading import Thread
 
 import boto3
 
@@ -13,6 +15,7 @@ class AWSMetrics(Metrics):
 AWSMetrics.reset()
 
 class AWSApiWrapper(object):
+    PAGE_SIZE = 1000
     def __init__(self):
         self.session = self.create_aws_session()
         self.resource = self.create_s3_resource()
@@ -28,16 +31,21 @@ class AWSApiWrapper(object):
 
     def get_s3_objects_by_bucket_name(self, bucket_name):
         bucket = self.get_s3_bucket_by_name(bucket_name)
-        return bucket.objects.all()
+        return bucket.objects.page_size(count=self.PAGE_SIZE)
 
 
 class S3Restore(object):
     '''This class wraps Boto3 Object endpoints, see official Boto3 documentation:
         http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Object
     '''
+    _sentinel = object()
+    queue = Queue()
+
     RestoreRequest = { 'Days' : 7 }
+
     def __init__(self):
        self.aws = AWSApiWrapper()
+       self.MAX_THREAD_COUNT = self.aws.PAGE_SIZE
     
     @classmethod
     @AWSMetrics.counter('number_of_objects')
@@ -59,13 +67,56 @@ class S3Restore(object):
         return s3_obj_detail.restore is None
 
     @classmethod
+    def reset(cls):
+        cls.queue = Queue()
+
+    @classmethod
     @AWSMetrics.counter('number_of_requests')
     def call_restore(cls, s3_obj_summary):
         return s3_obj_summary.restore_object(RestoreRequest=cls.RestoreRequest)
 
-    def bucket(self, bucket):
-        objects = self.aws.get_s3_objects_by_bucket_name(bucket)
-        for object in objects:
-           self.restore_s3_object(object)
+    @classmethod
+    def _start_threads(cls, threads):
+        for thread in threads: thread.start()
+        return threads
 
+    @classmethod
+    def _block_until_threads_finish(cls, threads):
+        for thread in threads: thread.join()
+        return threads
+
+    def bucket(self, bucket):
+        objects = self._get_s3_objects_by_bucket_name(bucket)
+        self._restore_objects_with_threads(objects)
+
+    def producer(self, objects):
+        for object in objects:
+            self.queue.put(object)
+        self.queue.put(self._sentinel)
+
+    def consumer(self):
+        while True:
+            data  = self.queue.get()
+            if data == self._sentinel:
+                self.queue.put(self._sentinel)
+                break
+            self.restore_s3_object(data)
+
+    def _restore_objects_with_threads(self, objects):
+        producer_thread = self._create_single_producer_thread(objects)
+        consumer_threads = self._create_consumer_threads()
+        
+        producer_thread.start()
+        self._start_threads(consumer_threads)
+
+        self._block_until_threads_finish(consumer_threads)
+
+    def _create_single_producer_thread(self, objects):
+        return Thread(target=self.producer, args=(objects,))
+
+    def _create_consumer_threads(self):
+        return [Thread(target=self.consumer) for _ in range(self.MAX_THREAD_COUNT)]
+
+    def _get_s3_objects_by_bucket_name(self, bucket):
+        return self.aws.get_s3_objects_by_bucket_name(bucket)
 
--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest, os, collections
+from Queue import Queue
 
 import boto3
 from mock import patch, Mock, MagicMock
@@ -23,7 +24,10 @@ class AWSApiWrapperTests(unittest.TestCase):
             self.assertEqual(test_credentials['AWS_SECRET_ACCESS_KEY'], credentials.secret_key)
 
 
-class S3RestoreTests
+class S3RestoreTests(unittest.TestCase):
+    def setUp(self):
+        S3Restore.reset()
+
     def test__we_can_identify_an_s3_object_with_glacier_storage_class(self):
         mock_s3_object = Mock()
         mock_s3_object.storage_class = 'GLACIER'
@@ -78,4 +82,55 @@ class S3RestoreTests
         restore.bucket('test bucket')
 
         self.assertEqual(s3_restore_call.call_count, 3)
+
+    @patch.object(AWSApiWrapper, 'get_s3_objects_by_bucket_name')
+    def test__we_can_add_all_objects_to_a_queue(self, aws_api_call):
+        mock_bucket_contents = [Mock(), Mock(), Mock()]
+        aws_api_call.return_value = mock_bucket_contents
+
+        restore = S3Restore()
+        restore.producer(mock_bucket_contents)
+
+        '''Q: why > 1? A: Becuase the queue is never empty, there is always a sentinel objeect in its tail'''
+        while restore.queue.qsize() > 1:
+            obj = restore.queue.get()
+            self.assertIn(obj, mock_bucket_contents)
+
+        thread_safe_queue = Queue
+        self.assertIsInstance(restore.queue, thread_safe_queue)
+
+    def test__the_object_queue_is_thread_safe(self):
+        restore = S3Restore()
+        thread_safe_queue = Queue
+        self.assertIsInstance(restore.queue, thread_safe_queue)
+
+    @patch.object(S3Restore, 'restore_s3_object')
+    def test__we_consume_all_objects_in_the_queue_until_there_are_none(self, s3_restore_call):
+        mock_bucket_contents = [Mock(), Mock(), Mock()]
+        restore = S3Restore()
+        restore.producer(mock_bucket_contents)
+
+        restore.consumer()
+
+        self.assertEqual(s3_restore_call.call_count, 3)
+        # there's one object left...
+        self.assertEqual(restore.queue.qsize(), 1)
+        # ...and that object is the sentinel, not an s3object
+        self.assertEqual(restore.queue.get(), restore._sentinel)
+
+    def test__we_create_a_single_producer_thread(self):
+        restore = S3Restore()
+        mock_bucket_contents = [Mock(), Mock(), Mock()]
+        producer_thread = restore._create_single_producer_thread(mock_bucket_contents)
+        # has producer target
+        self.assertEqual(producer_thread._Thread__target, restore.producer)
+
+    def test__we_create_as_many_consumer_threads_as_there_are_objects_in_an_aws_objects_page(self):
+        restore = S3Restore()
+        aws_objects_page_size = restore.aws.PAGE_SIZE
+
+        consumer_threads = restore._create_consumer_threads()
+        number_of_consumer_threads = len(consumer_threads)
+
+        self.assertEqual(number_of_consumer_threads, aws_objects_page_size)
 
