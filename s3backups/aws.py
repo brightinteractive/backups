f56cfa00661f6c3f6f6ee95978f1aa284113bf1e
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-23 16:36:43 +0100

    Add metric to record the http response status codes of restore requests

--- s3backups/aws.py
+++ s3backups/aws.py
@@ -10,7 +10,28 @@ from metrics import Metrics
 
 
 class AWSMetrics(Metrics):
-    available_metrics = ('number_of_objects', 'number_of_requests')
+    available_metrics = ('restore_object_status_codes', 'number_of_objects', 'number_of_requests')
+
+    @classmethod
+    def status_code(cls, metric):
+        if metric not in cls.available():
+            raise AttributeError('\'%s\' is not an available metric.'% metric)
+
+        def wrapper(fn):
+            def _status_code(*args, **kwargs):
+                response = fn(*args, **kwargs)
+                status_code = response['ResponseMetadata']['HTTPStatusCode']
+                with cls._lock:
+                    if cls.report[metric]:
+                        if cls.report[metric].has_key(status_code):
+                            cls.report[metric][status_code] += 1
+                        else:
+                            cls.report[metric][status_code] = 1
+                    else:
+                        cls.report[metric] = { status_code : 1 }
+                return response
+            return _status_code
+        return wrapper
 
 AWSMetrics.reset()
 
@@ -71,6 +92,7 @@ class S3Restore(object):
         cls.queue = Queue()
 
     @classmethod
+    @AWSMetrics.status_code('restore_object_status_codes')
     @AWSMetrics.counter('number_of_requests')
     def call_restore(cls, s3_obj_summary):
         return s3_obj_summary.restore_object(RestoreRequest=cls.RestoreRequest)
--- tests/test_aws_metrics.py
+++ tests/test_aws_metrics.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+
 import unittest
 
-from mock import patch, Mock
+from mock import patch, Mock, MagicMock
 
 from s3backups.aws import S3Restore, AWSApiWrapper, AWSMetrics
 
@@ -19,14 +20,13 @@ class AWSMetricsTests(unittest.TestCase):
 
         S3Restore.restore_s3_object(object())
         S3Restore.restore_s3_object(object())
-        S3Restore.restore_s3_object(object())
         number_of_recorded_objects = AWSMetrics.report['number_of_objects']
-        self.assertEqual(number_of_recorded_objects, 4)
+        self.assertEqual(number_of_recorded_objects, 3)
 
-    @patch.object(AWSApiWrapper, 'get_s3_objects_by_bucket_name')
-    def test__we_can_record_the_number_of_requests_made(self, mock_api):
+    def test__we_can_record_the_number_of_requests_made(self):
+        mock_response = self.mock_aws_response()
         mock_s3_object = Mock()
-        mock_s3_object.restore_object = Mock()
+        mock_s3_object.restore_object = Mock(return_value=mock_response)
 
         S3Restore.call_restore(mock_s3_object)
         number_of_requests = AWSMetrics.report['number_of_requests']
@@ -36,3 +36,22 @@ class AWSMetricsTests(unittest.TestCase):
         number_of_requests = AWSMetrics.report['number_of_requests']
         self.assertEqual(number_of_requests, 5)
 
+    def test__we_can_record_the_status_code_of_restore_reponses(self):
+        status_codes_and_their_frequencies = { '200': 12, '300': 11, '302': 1, '202': 1, '505': 1}
+
+        for status_code, number_of_responses in status_codes_and_their_frequencies.items():
+            for response in range(number_of_responses):
+                response = self.mock_aws_response(HTTPStatusCode=status_code)
+                mock_s3_object = Mock()
+                mock_s3_object.restore_object = MagicMock(return_value=response)
+                S3Restore.call_restore(mock_s3_object)
+
+        recorded_status_codes = AWSMetrics.report['restore_object_status_codes']
+        self.assertEqual(status_codes_and_their_frequencies, recorded_status_codes)
+
+    def mock_aws_response(self, **kwargs):
+        response = { 'HTTPStatusCode': '200',
+                'HostId': 'KX19tFhyBOCZfaDAk78nnkLiAvVV8fUgv11LMykrn3aXwZhJcimxZEbqRmUYaalq/beynjJ4Hmw=',
+                'RequestId': '609FF741F8386E27' }
+        response.update(kwargs)
+        return { 'ResponseMetadata': response }
