35b7f2a4455af7ec830a9fce3ff9239a559b2c11
anthonysgevans <anthony.s.g.evans@gmail.com>
2016-06-29 15:55:33 +0100

    Create setup file for pip module

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='s3backups',
+        version='0.1',
+        description='',
+        url='http://github.com/brightinteractive/s3backups',
+        author='Anthony S.G. Evans',
+        author_email='anthony@bright-interactive.com',
+        license='Copyright Bright Interactive',
+        packages=['s3backups'],
+        zip_safe=False)
